5bad6477607453b2adcd1f6dfefccd04918ae0e2
Adam Lawson <adamjlawson22@gmail.com>
2021-09-05 19:30:44 +0100

    RMX1971: Bringup PD
    Also define Palladium specific properties

--- palladium_RMX1971.mk
+++ palladium_RMX1971.mk
@@ -4,18 +4,27 @@
 # SPDX-License-Identifer: Apache-2.0
 #
 
-# Inherit some common Descendant stuff.
-$(call inherit-product, vendor/descendant/config/common_full_phone.mk)
+# Inherit some common Palladium stuff.
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
 
 # Inherit from RMX1971 device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+#GApps
 TARGET_BOOT_ANIMATION_RES := 1080
-#TARGET_GAPPS_ARCH := arm64
+TARGET_GAPPS_ARCH := arm64
+
+#PALLADIUM-PROPS
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.palladiumdevice.cpu=SDM712 \
+    ro.palladiumdevice.display=6.3 \
+    ro.palladiumdevice.displaytype=FULLHD+
+
+
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := RMX1971
-PRODUCT_NAME := descendant_RMX1971
+PRODUCT_NAME := palladium_RMX1971
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 5 Pro
 PRODUCT_MANUFACTURER := Realme
